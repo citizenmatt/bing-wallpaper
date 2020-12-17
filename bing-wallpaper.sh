1ee1d20ec424499690075b89b45c102f5b8d28b4
Matt Ellis <m.t.ellis@gmail.com>
2020-12-17 10:36:47 +0000

    Support downloading images from all markets at once

--- bing-wallpaper.sh
+++ bing-wallpaper.sh
@@ -4,6 +4,9 @@
 readonly SCRIPT=$(basename "$0")
 readonly VERSION='0.4.0'
 readonly RESOLUTIONS=(1920x1080 800x480 400x240)
+readonly PAGE_SIZE=8
+readonly MAX_BOOST=16
+readonly ALL_MARKETS=( "ar-XA" "bg-BG" "cs-CZ" "da-DK" "de-AT" "de-CH" "de-DE" "el-GR" "en-AU" "en-CA" "en-GB" "en-ID" "en-IE" "en-IN" "en-MY" "en-NZ" "en-PH" "en-SG" "en-US" "en-XA" "en-ZA" "es-AR" "es-CL" "es-ES" "es-MX" "es-US" "es-XL" "et-EE" "fi-FL" "fr-BE" "fr-CA" "fr-CH" "fr-FR" "he-IL" "hr-HR" "hu-HU" "it-IT" "ja-JP" "ko-KR" "lt-LT" "lv-LV" "nb-NO" "nl-BE" "nl-NL" "pl-PL" "pt-BR" "pt-PT" "ro-RO" "ru-RU" "sk-SK" "sl-SL" "sv-SE" "th-TH" "tr-TR" "uk-UA" "zh-CN" "zh-HK" "zh-TW" )
 
 usage() {
 cat <<EOF
@@ -18,7 +21,8 @@ Options:
   -s --ssl                       Communicate with bing.com over SSL.
   -b --boost <n>                 Use boost mode. Try to fetch latest <n> pictures.
   -q --quiet                     Do not display log messages.
-  -m --market <market name>      Name of the market to retrieve image for. Defaults to none.
+  -m --market <market name|all>  Name of the market to retrieve image for. Defaults to none.
+                                 Use "all" to download images from all markets.
   -n --filename <file name>      The name of the downloaded picture. Defaults to
                                  the upstream name.
   -p --picturedir <picture dir>  The full path to the picture download dir.
@@ -44,8 +48,9 @@ transform_urls() {
         tr "\n" " "
 }
 
-fetch_pictures() {
+fetch_urls() {
     if [ ! -z "$1" ]; then
+        echo "Fetching URLs for $1"
         loc_mkt_param="&mkt=$1"
     fi
 
@@ -62,28 +67,11 @@ fetch_pictures() {
         loc_boost=$((loc_boost-PAGE_SIZE))
         loc_idx=$((loc_idx+PAGE_SIZE))
     done
-
-    for p in "${urls[@]}"; do
-        if [ -z "$FILENAME" ]; then
-            # Extract OHR.ElbeBastei_EN-GB1140600783_1920x1080.jpg from id. Strip `OHR.` then strip culture
-            filename=$(echo "$p" | sed -e 's/.*[?&;]id=\([^&]*\).*/\1/' -e 's/^[^\.]*\.//' -e 's/_.*_/_/')
-        else
-            filename="$FILENAME"
-        fi
-        if [ -n "$FORCE" ] || [ ! -f "$PICTURE_DIR/$filename" ]; then
-            print_message "Downloading: $filename..."
-            curl $CURL_QUIET -Lo "$PICTURE_DIR/$filename" "$p"
-        else
-            print_message "Skipping: $filename..."
-        fi
-    done
 }
 
 # Defaults
 PICTURE_DIR="$HOME/Pictures/bing-wallpapers/"
 RESOLUTION="1920x1080"
-PAGE_SIZE=8
-MAX_BOOST=16
 
 # Option parsing
 while [[ $# -gt 0 ]]; do
@@ -155,7 +143,28 @@ if [ $BOOST -gt $MAX_BOOST ]; then
     BOOST=$MAX_BOOST
 fi
 
-fetch_pictures "$MARKET"
+if [ "$MARKET" == "all" ]; then
+    for m in "${ALL_MARKETS[@]}"; do
+        fetch_urls $m
+    done
+else
+    fetch_urls "$MARKET"
+fi
+
+for p in "${urls[@]}"; do
+    if [ -z "$FILENAME" ]; then
+        # Extract OHR.ElbeBastei_EN-GB1140600783_1920x1080.jpg from id. Strip `OHR.` then strip culture
+        filename=$(echo "$p" | sed -e 's/.*[?&;]id=\([^&]*\).*/\1/' -e 's/^[^\.]*\.//' -e 's/_.*_/_/')
+    else
+        filename="$FILENAME"
+    fi
+    if [ -n "$FORCE" ] || [ ! -f "$PICTURE_DIR/$filename" ]; then
+        print_message "Downloading: $filename..."
+        curl $CURL_QUIET -Lo "$PICTURE_DIR/$filename" "$p"
+    else
+        print_message "Skipping: $filename..."
+    fi
+done
 
 if [ -n "$SET_WALLPAPER" ]; then
     /usr/bin/osascript<<END
